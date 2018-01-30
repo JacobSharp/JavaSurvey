1797b6dfd80d5412627eb29fe5f106742921d231
zsharj <zsharj@WS-ZSHARJ.bbcnet-zh.local>
2018-01-30 08:45:48 +0100

    add: eine JFrame erstellt

--- JavaSurvey/src/javaSurvey/JavaSurvey.java
+++ JavaSurvey/src/javaSurvey/JavaSurvey.java
@@ -1,7 +1,34 @@
 package javaSurvey;
 
-public class JavaSurvey {
+import java.awt.*;
+import javax.swing.*;
+
+public class JavaSurvey extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 420428732984733599L;
+	private JTextField user = new JTextField("Username");
+	private JPasswordField pass = new JPasswordField("Password");
+	private JButton login = new JButton("Login");
+	
+	public JavaSurvey() {
+		
+		setLayout(new BorderLayout());
+		add(user, BorderLayout.NORTH);
+		add(pass, BorderLayout.CENTER);
+		add(login, BorderLayout.SOUTH);		
+		setSize(200, 100);
+		
+	}
 	
 	
+	public static void main(String[] args) {
+		
+		JavaSurvey launcher = new JavaSurvey();
+		launcher.setVisible(true);
+		
+	}
 	
 }
