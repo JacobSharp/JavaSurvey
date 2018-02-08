b1d0a70d0a8c53b2bb378d2e51e61b2ed1af454f
zsharj <zsharj@WS-ZSHARJ.bbcnet-zh.local>
2018-02-08 14:24:56 +0100

    edit: formatiert

--- JavaSurvey/src/javaSurvey/ProfileJDBCDao.java
+++ JavaSurvey/src/javaSurvey/ProfileJDBCDao.java
@@ -9,41 +9,41 @@ public class ProfileJDBCDao implements ProfileDao {
 
 	@Override
 	public Profile findProfileByUser(int id) {
-			String sql = "select * from javasurveys.profile where user_id = ?";
-			Connection con = ConnectionFactory.getInstance().getConnection();
-			PreparedStatement ps = null;
-			ResultSet rs = null;
-			Profile profile = null;
+		String sql = "select * from javasurveys.profile where user_id = ?";
+		Connection con = ConnectionFactory.getInstance().getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Profile profile = null;
 
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				profile = new Profile();
+				profile.setId(rs.getInt("id"));
+				profile.setName(rs.getString("name"));
+				profile.setGeburtsdatum(rs.getDate("geburtsdatum"));
+				profile.setProfilBildLink(rs.getString("profilBildLink"));
+				profile.setUser_id(rs.getInt("user_id"));
+				break;
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException("Error occurred while searching for your profile: ", e);
+		} finally {
 			try {
-				ps = con.prepareStatement(sql);
-				ps.setInt(1, id);
-				rs = ps.executeQuery();
-
-				while (rs.next()) {
-					profile = new Profile();
-					profile.setId(rs.getInt("id"));
-					profile.setName(rs.getString("name"));
-					profile.setGeburtsdatum(rs.getDate("geburtsdatum"));
-					profile.setProfilBildLink(rs.getString("profilBildLink"));
-					profile.setUser_id(rs.getInt("user_id"));
-					break;
+				if (ps != null) {
+					ps.close();
+				}
+				if (rs != null) {
+					rs.close();
 				}
 			} catch (SQLException e) {
-				throw new RuntimeException("Error occurred while searching for your profile: ", e);
-			} finally {
-				try {
-					if (ps != null) {
-						ps.close();
-					}
-					if (rs != null) {
-						rs.close();
-					}
-				} catch (SQLException e) {
-					throw new RuntimeException("We are sorry. A technical error occurred. Please try again later.", e);
-				}
+				throw new RuntimeException("We are sorry. A technical error occurred. Please try again later.", e);
 			}
-			return profile;
+		}
+		return profile;
 	}
 
 }
