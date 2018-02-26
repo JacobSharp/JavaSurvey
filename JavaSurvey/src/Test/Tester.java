0f4f718a662c58bd45fab33cf24fbeeb5ceddd82
zredza <zredza@WS-ZREDZA.bbcnet-zh.local>
2018-02-26 14:53:41 +0100

    rsdvbrtgb

---
+++ JavaSurvey/src/Test/Tester.java
@@ -0,0 +1,37 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+
+public class Tester {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test1() {
+		Assert.assertEquals(3, 1+2);
+	}
+
+}
